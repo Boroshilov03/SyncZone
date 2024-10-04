19e833fa2c91a319682925bcb49219987d192c71
durngyn <darrennguyen321919@gmail.com>
2024-10-03 20:39:02 -0700

    making SyncZone title in SignIn

--- frontend/src/screens/SignInScreen.jsx
+++ frontend/src/screens/SignInScreen.jsx
@@ -3,6 +3,8 @@ import { Alert, StyleSheet, View, Text } from "react-native";
 import { supabase } from "../lib/supabase";
 import { Button, Input } from "@rneui/themed";
 import useStore from "../store/store";
+import GradientText from "react-native-gradient-texts";
+
 
 export default function SignInScreen({ navigation }) { // Ensure navigation prop is here
   const { setAuthenticated, setUser, setAccessToken, setRefreshToken } = useStore();
@@ -34,52 +36,83 @@ export default function SignInScreen({ navigation }) { // Ensure navigation prop
   }
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: "font-awesome", name: "envelope" }}
-          onChangeText={setEmail}
-          value={email}
-          placeholder="email@address.com"
-          autoCapitalize="none"
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: "font-awesome", name: "lock" }}
-          onChangeText={setPassword}
-          value={password}
-          secureTextEntry
-          placeholder="Password"
-          autoCapitalize="none"
-        />
-      </View>
-      <View>
-        <Button
-          title="Sign in"
-          disabled={loading}
-          onPress={signInWithEmail}
-        />
-      </View>
-      <Text style={styles.signInText}>
-        Don't have an account?{" "}
-        <Text
-          style={styles.signInLink}
-          onPress={() => navigation.navigate("SignUp")}
-        >
-          Sign Up
+
+    <View style={styles.parent}>
+      <View style={styles.container}>
+        <View style={[styles.verticallySpaced, styles.mt20]}>
+          <Input
+            label="Email"
+            leftIcon={{ type: "font-awesome", name: "envelope" }}
+            onChangeText={setEmail}
+            value={email}
+            placeholder="email@address.com"
+            autoCapitalize="none"
+          />
+        </View>
+        <View style={styles.verticallySpaced}>
+          <Input
+            label="Password"
+            leftIcon={{ type: "font-awesome", name: "lock" }}
+            onChangeText={setPassword}
+            value={password}
+            secureTextEntry
+            placeholder="Password"
+            autoCapitalize="none"
+          />
+        </View>
+        <View>
+          <Button
+            title="Sign in"
+            disabled={loading}
+            onPress={signInWithEmail}
+          />
+        </View>
+        <Text style={styles.signInText}>
+          Don't have an account?{" "}
+          <Text
+            style={styles.signInLink}
+            onPress={() => navigation.navigate("SignUp")}
+          >
+            Sign Up
+          </Text>
         </Text>
-      </Text>
+      </View>
+      <View style={styles.box}>
+        <GradientText
+          text={"SyncZone"}
+          textAlign={""}
+          fontSize={40}
+          isGradientFill
+          isGradientStroke
+          width={420}
+          locations={{ x: 210, y: 65 }}
+          gradientColors={["#D49AC0", "#6FD2E2"]}
+          fontFamily={"Gill Sans"}
+        />
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     marginTop: 40,
     padding: 12,
+    // backgroundColor: '#8ca599',
+  },
+  parent: {
+    flex: 1,
+    marginTop: 40,
+    padding: 12,
+    // backgroundColor: '#8ca599',
+  },
+  box: {
+    // backgroundColor: '#fff',
+    marginTop: 40,
+    padding: 12,
+    alignItems: "center",
   },
   mt20: {
     marginTop: 20,
@@ -91,5 +124,10 @@ const styles = StyleSheet.create({
   signInLink: {
     color: "#007BFF",
     fontWeight: "bold",
+  },
+  sync: {
+    textAlign: "center",
+    fontWeight: 'bold',
+    fontSize: 30,
   },
 });
