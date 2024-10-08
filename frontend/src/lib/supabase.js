c8871899b2f4b891c3289d1b93e507ded67e096c
durngyn <darrennguyen321919@gmail.com>
2024-10-07 22:20:11 -0700

    supa

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -5,9 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 import { REACT_APP_SUPABASE_URL, REACT_APP_ANON_KEY } from "@env";
 
 const supabaseUrl = REACT_APP_SUPABASE_URL;
-//"https://yjsyzptnadinqxzmkgdh.supabase.co"
-const supabaseAnonKey = REACT_APP_ANON_KEY;//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqc3l6cHRuYWRpbnF4em1rZ2RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwNDAzMTEsImV4cCI6MjA0MjYxNjMxMX0.jfGIoX3U5Klw71J05I433wTZ02uJKIZlhbkmoyGZZh0"
-
+const supabaseAnonKey = REACT_APP_ANON_KEY;
 
 console.log(supabaseUrl);
 console.log(supabaseAnonKey);
