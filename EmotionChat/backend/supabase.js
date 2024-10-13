7c89697d66889e9fdc3b92dd1bb8471a930775e1
Jake <124315722+backhs97@users.noreply.github.com>
2024-10-13 01:59:42 -0700

    Add files via upload

--- EmotionChat/backend/supabase.js
+++ EmotionChat/backend/supabase.js
@@ -1,6 +1,28 @@
-import { createClient } from '@supabase/supabase-js';
+import { AppState } from "react-native";
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import { REACT_APP_SUPABASE_URL, REACT_APP_ANON_KEY } from "@env";
 
-const SUPABASE_URL = 'URL';
-const SUPABASE_API_KEY = 'API';
+const supabaseUrl = "https://yjsyzptnadinqxzmkgdh.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqc3l6cHRuYWRpbnF4em1rZ2RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwNDAzMTEsImV4cCI6MjA0MjYxNjMxMX0.jfGIoX3U5Klw71J05I433wTZ02uJKIZlhbkmoyGZZh0";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+console.log(supabaseUrl);
+console.log(supabaseAnonKey);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
+
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
