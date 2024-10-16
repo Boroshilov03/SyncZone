e3f554b35143ea959e541ff47518b20198cfcd3c
Armacool <brianthi323851@gmail.com>
2024-10-15 23:44:58 -0700

    Merg

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -4,11 +4,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { REACT_APP_SUPABASE_URL, REACT_APP_ANON_KEY } from "@env";
 
+const supabaseUrl = "https://yjsyzptnadinqxzmkgdh.supabase.co/";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqc3l6cHRuYWRpbnF4em1rZ2RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwNDAzMTEsImV4cCI6MjA0MjYxNjMxMX0.jfGIoX3U5Klw71J05I433wTZ02uJKIZlhbkmoyGZZh0";
 
-const supabaseUrl = REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = REACT_APP_ANON_KEY;
-
-
+console.log(supabaseUrl);
 console.log(supabaseAnonKey);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
