64ed576eb91d27e89f1111c0bc0c0749490f68b6
backhs97 <backhs97@gmail.com>
2024-10-22 14:21:47 -0700

    update env

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -2,10 +2,10 @@ import { AppState } from "react-native";
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-import { REACT_APP_SUPABASE_URL, REACT_APP_ANON_KEY } from "@env";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
-const supabaseUrl = REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = REACT_APP_ANON_KEY;
+const supabaseUrl = SUPABASE_URL;
+const supabaseAnonKey = SUPABASE_ANON_KEY;
 
 console.log(supabaseAnonKey);
 
