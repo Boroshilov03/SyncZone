95fb0ec696b6289143f7b5a701ca3aac4dad0f64
durngyn <darrennguyen321919@gmail.com>
2024-11-04 01:37:16 -0800

    sign in/up

--- frontend/src/screens/SplashScreen.jsx
+++ frontend/src/screens/SplashScreen.jsx
@@ -1,14 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import SpinningLogo from "../components/SpinningLogo";
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <SpinningLogo />
     </View>
   )
 }
 
 export default SplashScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+})
